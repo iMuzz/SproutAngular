9bcd01f160d3f755fe5c125424e862029a44369d
Faraaz Nishtar <faraaznishtar@gmail.com>
2015-05-27 16:36:38 -0700

    jade partial working correctly

--- public/app/app.js
+++ public/app/app.js
@@ -1,32 +1,29 @@
 (function(){
 	var app = angular.module('dashboard', ['ngResource', 'ngRoute']);
 	
-	// app.config(function($routeProvider, $locationProvider){
-	// 	$locationProvider.html5Mode({
-	// 		enabled: true,
-	// 		requireBase: false
-	// 	});
-	// 	$routeProvider
-	// 		.when('/', 
-	// 			{
-	// 				templateUrl: 'main', 
-	// 				controller: 'mainCtrl'
-	// 			}
-	// 		);
-	// });
+	app.config(function($routeProvider, $locationProvider){
+		$locationProvider.html5Mode(true);
+		$routeProvider
+			.when('/', 
+				{
+					templateUrl: '/partials/main.jade', 
+					controller: 'mainCtrl'
+				}
+			);
+	});
 
 	app.controller('mainCtrl', function($scope){
-		$scope.someVar = "Hello Angular"; 
+		$scope.someVar = "Does this even work?"; 
 	});
 
-	app.directive('testDirective', function(){
-		return {
-			restrict: 'E',
-			templateUrl: 'partials/main.jade',
-			controller: function(scope){
-				console.log("Test");
-			}
-		}
-	});
+	// app.directive('testDirective', function(){
+	// 	return {
+	// 		restrict: 'A',
+	// 		templateUrl: 'partials/main.jade',
+	// 		controller: function(scope){
+	// 			console.log("Test");
+	// 		}
+	// 	}
+	// });
 
 })();
--- server/server.js
+++ server/server.js
@@ -25,7 +25,8 @@ app.use(express.static(path.join(__dirname, '../public')));
 // app.use('/users', users);
 
 app.get('/partials/:partialPath', function(req, res, next){
-    res.render('partials/' + req.params.partialsPath);
+    console.log('partials/' + req.params.partialPath);
+    res.render('partials/' + req.params.partialPath);
 });
 app.get('*', function(req, res){
     res.render('index');
