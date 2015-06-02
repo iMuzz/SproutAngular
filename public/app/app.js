187a4d4b52cc1acdaf1db95f5a6a91449c06d8a4
Faraaz Nishtar <faraaznishtar@gmail.com>
2015-06-01 23:59:32 -0700

    userData factory currently not working

--- public/app/app.js
+++ public/app/app.js
@@ -11,9 +11,20 @@ app.config(function($routeProvider, $locationProvider){
 		);
 });
 
-app.controller('dashCtrl', function($scope){
+// app.factory('UserDataFactory', ['', function(){
+// 	return {
+// 		userName: "Julia Winehart",
+// 		loanBalance: 19852,
+// 		creditScore: 649,
+// 		plantScore: 649
+// 	};
+// }]);
+
+app.controller('dashCtrl', ['$scope', function($scope){
+	// $scope.user = UserDataFactory;
+
 	$scope.userName = "Julia Winehart";
 	$scope.loanBalance = 19852;
 	$scope.creditScore = 649;
 	$scope.plantScore = 649;
-});
+}]);
---
+++ public/app/services/userData.js
@@ -0,0 +1,8 @@
+// app.factory('userData', ['', function(){
+// 	return {
+// 		userName: "Julia Winehart",
+// 		loanBalance: 19852,
+// 		creditScore: 649,
+// 		plantScore: 649
+// 	};
+// }])
