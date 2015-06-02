bba5dc4fd958054dbab2ecf076e8c9b2548fea02
Faraaz Nishtar <faraaznishtar@gmail.com>
2015-06-02 02:17:30 -0700

    userData factory working defined in app.js file

--- public/app/app.js
+++ public/app/app.js
@@ -11,20 +11,15 @@ app.config(function($routeProvider, $locationProvider){
 		);
 });
 
-// app.factory('UserDataFactory', ['', function(){
-// 	return {
-// 		userName: "Julia Winehart",
-// 		loanBalance: 19852,
-// 		creditScore: 649,
-// 		plantScore: 649
-// 	};
-// }]);
+app.factory('UserDataFactory', function(){
+	return {
+		userName: "Julia Winehart",
+		loanBalance: 19852,
+		creditScore: 649,
+		plantScore: 649
+	};
+});
 
-app.controller('dashCtrl', ['$scope', function($scope){
-	// $scope.user = UserDataFactory;
-
-	$scope.userName = "Julia Winehart";
-	$scope.loanBalance = 19852;
-	$scope.creditScore = 649;
-	$scope.plantScore = 649;
+app.controller('dashCtrl', ['$scope', '$http','UserDataFactory', function($scope, $http, UserDataFactory){
+	$scope.user = UserDataFactory;
 }]);
