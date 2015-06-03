b4607b3c38a2904cfef5cf6bef962eb0bd9e19b0
Faraaz Nishtar <faraaznishtar@gmail.com>
2015-06-02 17:56:18 -0700

    video playing to second when plantScore gets increased

--- public/app/plantAnimation/animateVideo.js
+++ public/app/plantAnimation/animateVideo.js
@@ -3,27 +3,57 @@ app.directive('videoPanel', function(){
 		restrict: 'E',
 		templateUrl: '/partials/videoPanel.jade', 
 		scope: {
-			videoUrl: '@videourl',
-			percentage: '=percentage'
+			videoUrl: '@videourl'
 		},
-		controller: function($scope){
-			$scope.playToPerc = $scope.percentage / 850;
-		},
+
+		link: function(scope, el, attrs) {
+			// scope.playToPerc = attrs['percentage'] / 850;
+			// var video = el.find('video')[0];
+
+			// setTimeout(function(){ 
+			// 	scope.vidLength = video.duration;
+			// 	resumeToSec(scope.playToPerc * scope.vidLength);
+			// 	// resumeToSec(2);
+			// 	video.play();
+			// }, 1000);
+
+			// var resumeToSec = function(stopTime){
+			// 	scope.endTime = stopTime;
+			// 	video.addEventListener("timeupdate", pauseAtTime);
+
+			// 	video.play();
+			// }
+
+			// var pauseAtTime = function(){
+			// 	if (video.currentTime >= scope.endTime) {
+			// 		video.pause();
+			// 		video.removeEventListener("timeupdate", pauseAtTime)
+			// 	};
+			// }
+		}
 	}
 });
 
-app.directive('startVideo', function(){
+app.directive('testDirective', function(){
 	return {
 		restrict: 'A',
-		link: function(scope, el, attrs){
-			var video = el[0];
+		link: function(scope, el, attrs) {
+			var MAX_CREDIT_SCORE = 850;
+
+			var calculatePercentage = function(creditScore) {
+				return creditScore / MAX_CREDIT_SCORE;
+			}
+
+			console.log(attrs['percentage']);
+			scope.playToPerc = calculatePercentage(attrs['percentage']);
+			var video = el.find('video')[0];
 
 			setTimeout(function(){ 
 				scope.vidLength = video.duration;
 				resumeToSec(scope.playToPerc * scope.vidLength);
-				// resumeToSec(2);
 				video.play();
 			}, 1000);
+
 
 			var resumeToSec = function(stopTime){
 				scope.endTime = stopTime;
@@ -38,6 +68,10 @@ app.directive('startVideo', function(){
 					video.removeEventListener("timeupdate", pauseAtTime)
 				};
 			}
+
+			scope.$watch(attrs['testDirective'], function(newVal, oldVal){
+				resumeToSec(calculatePercentage(newVal));
+			});
 		}
 	}
 });
