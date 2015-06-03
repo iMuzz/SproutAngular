fe6539b5f1409361f12ff8434fb5e20f7b2fd1f3
Faraaz Nishtar <faraaznishtar@gmail.com>
2015-06-03 02:48:40 -0700

    active class getting added and removed on click

--- public/app/creditSimulation/simulationButtonDirective.js
+++ public/app/creditSimulation/simulationButtonDirective.js
@@ -3,10 +3,32 @@ app.directive('simulationButton', function(){
 		restrict: 'E',
 		templateUrl: '/partials/simulationButton.jade',
 		scope: {
-			scoreChange: "@"
+			scoreChange: "@",
+			onActivate: "&method"
 		},
-		controller: function($scope) {
-			console.log("This is the first button: "+ $scope.scoreChange);
+		link: function(scope, el, attrs) {
+			var _el = el;
+			var active = false;
+			scope.activateButton = function(){
+				if (active) {
+					el.find(".simulation").removeClass("sim-active")
+					active = false
+					// scope.onActivate();
+				} else {
+					el.find(".simulation").addClass("sim-active")
+					active = true;
+					scope.onActivate();
+				};
+			};
 		}
 	}
 });
+
+// app.directive('clickActivator', function(){
+// 	return {
+// 		restrict: 'A',
+// 		link: function(scope, el, attrs) {
+
+// 		}
+// 	}
+// });
