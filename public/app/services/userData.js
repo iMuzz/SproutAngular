d0e5f994c84f1866d4484d81db26fadcc9bd34e9
Faraaz Nishtar <faraaznishtar@gmail.com>
2015-06-03 20:40:47 -0700

    change score

--- public/app/services/userData.js
+++ public/app/services/userData.js
@@ -2,11 +2,11 @@ app.service('UserData', function(){
 	
 	this.userName =  "Julia Winehart",
 	this.loanBalance =  19852,
-	this.creditScore =  100,
+	this.creditScore =  750,
 	this.payments = 10,
 	this.inquiries = 3,
 	this.openAccounts = 8,
-	this.plantScore = 100,
+	this.plantScore = 750,
 	this.scoreChange = 35;
 	
 	this.changePlantScore = function(scoreChange){
