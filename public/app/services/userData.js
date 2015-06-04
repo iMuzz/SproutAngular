1c6e33b4b41d0ae206f90f261c9d79509f12c8c4
Faraaz Nishtar <faraaznishtar@gmail.com>
2015-06-04 15:26:32 -0700

    change simButton values to appropriate score change values

--- public/app/services/userData.js
+++ public/app/services/userData.js
@@ -2,11 +2,11 @@ app.service('UserData', function(){
 	
 	this.userName =  "Julia Winehart",
 	this.loanBalance =  19852,
-	this.creditScore =  750,
+	this.creditScore =  650,
 	this.payments = 10,
 	this.inquiries = 3,
 	this.openAccounts = 8,
-	this.plantScore = 750,
+	this.plantScore = this.creditScore,
 	this.scoreChange = 35;
 	
 	this.changePlantScore = function(scoreChange){
