089868972d4a539dcb00ef05508ac381bc9a0057
Faraaz Nishtar <faraaznishtar@gmail.com>
2015-06-03 18:48:25 -0700

    convert data in mainContent to be dynamically loaded from userData service

--- public/app/services/userData.js
+++ public/app/services/userData.js
@@ -1,9 +1,13 @@
 app.service('UserData', function(){
 	
-	this.plantScore = 100;
 	this.userName =  "Julia Winehart",
 	this.loanBalance =  19852,
 	this.creditScore =  100,
+	this.payments = 10,
+	this.inquiries = 3,
+	this.openAccounts = 8,
+	this.plantScore = 100,
+	this.scoreChange = 35;
 	
 	this.changePlantScore = function(scoreChange){
 		this.plantScore += scoreChange;
